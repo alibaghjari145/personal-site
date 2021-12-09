55292078e845b440afaab1095ceed1051c63bdb6
ali baghjari <70961525+alibaghjari145@users.noreply.github.com>
2021-12-09 19:29:45 +0330

    Add files via upload
    Added new fiture

--- src/cmp/skills.jsx
+++ src/cmp/skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './navbar'
+import Typist from 'react-typist';
 import SkillBox from './skillbox'
 import js from './../img/js.svg'
 import html from './../img/html.svg'
@@ -16,29 +16,29 @@ class Skills extends React.Component {
         super(props);
         this.state = {
             box: [
-            
+
             ],
-            Array:[
+            Array: [
                 { id: 1, title: 'Reactjs', img: <img src={react} alt="js" />, color: '#61dafb', pers: '60' },
                 { id: 2, title: 'javascript', img: <img src={js} alt="js" />, color: '#f7df1e', pers: '75' },
                 { id: 3, title: 'HTML', img: <img src={html} alt="js" />, color: '#e44d26', pers: '80' },
                 { id: 4, title: 'css', img: <img src={css} alt="js" />, color: '#2196f3', pers: '85' },
                 { id: 5, title: 'Tailwind', img: <img src={tailwind} alt="js" />, color: '#45aab3', pers: '70' },
                 { id: 6, title: 'Sass', img: <img src={sass} alt="js" />, color: '#cf649a', pers: '65' },
-                { id: 7, title: 'Git', img: <img src={git} alt="js" />, color: '#e34c26 ', pers: '40' }, 
-                
+                { id: 7, title: 'Git', img: <img src={git} alt="js" />, color: '#e34c26 ', pers: '40' },
+
             ]
 
         }
     }
-   
-    componentDidMount(){
+
+    componentDidMount() {
         setInterval(() => {
-            if(this.state.Array.length>this.state.box.length){
+            if (this.state.Array.length > this.state.box.length) {
                 let i = this.state.box.length;
-                let box=[...this.state.box];
-                 box.push(this.state.Array[i]);
-                this.setState({box})
+                let box = [...this.state.box];
+                box.push(this.state.Array[i]);
+                this.setState({ box })
             }
         }, 300);
     }
@@ -47,7 +47,11 @@ class Skills extends React.Component {
         const { box } = this.state;
         return (
             <div className='skills content'>
-                <h2>The most importants:</h2>
+                <h2>
+                    <Typist avgTypingDelay={120} className='my-title' >
+                        The most importants:
+                    </Typist>
+                </h2>
                 {box.map(
                     box => (<SkillBox
                         key={box.id}
@@ -55,9 +59,9 @@ class Skills extends React.Component {
                         pers={box.pers}
                         color={box.color}
                         id={box.id}
-                        >
+                    >
                         {box.img}
-                       
+
 
                     </SkillBox>)
                 )}
