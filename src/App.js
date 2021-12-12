d8f7395620025360660bb31ba45574b197161f0a
ali baghjari <70961525+alibaghjari145@users.noreply.github.com>
2021-12-12 20:15:17 +0330

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './cmp/navbar';
 import About from './cmp/about';
-import { Route, Routes, useRoutes } from "react-router";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Experiences from './cmp/experiences';
 import Home from './cmp/home';
 import Skills from './cmp/skills';
@@ -9,30 +9,45 @@ import Contacts from './cmp/contact';
 
 
 function App() {
-    return ( <
-        Routes >
-        <
-        Route strict path = '/About'
-        element = {
-            [ < About / > , < Navbar / > ] }
-        /> <
-        Route path = '/Contacts'
-        element = {
-            [ < Contacts / > , < Navbar / > ] }
-        /> <
-        Route path = '/Experiences'
-        element = {
-            [ < Experiences / > , < Navbar / > ] }
-        /> <
-        Route path = '/Skills'
-        element = {
-            [ < Skills / > , < Navbar / > ] }
-        /> <
-        Route strict path = '/'
-        element = {
-            [ < Home / > , < Navbar / > ] }
-        /> <
-        /Routes>
+    return (<Routes >
+
+        <Route strict path='/About'
+            element={
+                [< About />, < Navbar />]
+            }
+        />
+
+        <Route path='/Contacts'
+            element={
+                [< Contacts />, < Navbar />]
+            }
+        />
+
+        <Route path='/Experiences'
+            element={
+                [< Experiences />, < Navbar />]
+            }
+        />
+
+        <Route path='/Skills'
+            element={
+                [< Skills />, < Navbar />]
+            }
+        />
+
+        <Route strict path='/home'
+            element={
+                [< Home />, < Navbar />]
+            }
+        />
+        
+        <Route strict path='/'
+            element={
+                [< Home />, < Navbar />]
+            }
+        />
+    
+    </Routes>
     );
 }
 
